d7dc6aec19fe4d19e571fcc766d0e6b8b306a5a3
sun624 <67940809+sun624@users.noreply.github.com>
2020-11-24 15:44:08 -0800

    Entity framework

--- Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Controllers/ButterflyController.cs
+++ Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Controllers/ButterflyController.cs
@@ -6,6 +6,7 @@ using ButterfliesShop.Models;
 using ButterfliesShop.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace ButterfliesShop.Controllers
 {
@@ -35,17 +36,78 @@ namespace ButterfliesShop.Controllers
             }
         }
 
+        public IActionResult Index()
+        {
+            IndexViewModel indexViewModel = new IndexViewModel();
+            indexViewModel.Butterflies = _data.ButterfliesList;
+            return View(indexViewModel);
+        }
+
         public IActionResult GetImage(int id)
         {
             Butterfly requestedButterfly = _data.GetButterflyById(id);
             if (requestedButterfly != null)
             {
-                return null;
+                string webRootpath = _environment.WebRootPath;
+                string folderPath = "\\images\\";
+                string fullPath = webRootpath + folderPath + requestedButterfly.ImageName;
+                if (System.IO.File.Exists(fullPath))
+                {
+                    FileStream fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                    byte[] fileBytes;
+                    using (BinaryReader br = new BinaryReader(fileOnDisk))
+                    {
+                        fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                    }
+                    return File(fileBytes, requestedButterfly.ImageMimeType);
+                }
+                else
+                {
+                    if (requestedButterfly.PhotoFile.Length > 0)
+                    {
+                        return File(requestedButterfly.PhotoFile, requestedButterfly.ImageMimeType);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
             }
             else
             {
                 return NotFound();
             }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Butterfly butterfly)
+        {
+            if (ModelState.IsValid)
+            {
+                Butterfly lastButterfly = _data.ButterfliesList.LastOrDefault();
+                butterfly.CreatedDate = DateTime.Today;
+                if (butterfly.PhotoAvatar != null && butterfly.PhotoAvatar.Length > 0)
+                {
+                    butterfly.ImageMimeType = butterfly.PhotoAvatar.ContentType;
+                    butterfly.ImageName = Path.GetFileName(butterfly.PhotoAvatar.FileName);
+                    butterfly.Id = lastButterfly.Id + 1;
+                    _butterfliesQuantityService.AddButterfliesQuantityData(butterfly);
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        butterfly.PhotoAvatar.CopyTo(memoryStream);
+                        butterfly.PhotoFile = memoryStream.ToArray();
+                    }
+                    _data.AddButterfly(butterfly);
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(butterfly);
         }
     }
 }
